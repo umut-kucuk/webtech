0537f11664c4ea9c91e13b99532cf31ae46cab3d
umut-kucuk <56439349+umut-kucuk@users.noreply.github.com>
2023-11-26 02:40:50 +0100

    Update main.js

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -1,9 +1,9 @@
 $(function(){
-  $("#custom-navbar").load("../resources/html/navbar.html");
+  $("#custom-navbar").load("webtech/resources/html/navbar.html");
 });
 
 $(function(){
-  $("#custom-foot").load("../resources/html/footer.html");
+  $("#custom-foot").load("webtech/resources/html/footer.html");
 });
 
 function selectStyle(style) {
